3f1991eb84eebcb94d66f816362de1c4dc441b2a
JEug480 <82353232+JEug480@users.noreply.github.com>
2022-10-01 19:22:21 -0400

    Invert a Matrix
    First create a makecachematrix and solve by inverting the matrix. Second using the inverted matrix to returned it of x.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+## There are two funtions that will Cache an inverse coming 
+## of  a matrix 
 
-## Write a short comment describing this function
+## This function create a matrix that solve for inverse 
 
 makeCacheMatrix <- function(x = matrix()) {
+      s <- NULL
+      set <- function (y)
+      x <<- y
+      s <<- NULL
 
 }
+  get <- funtion () x
+  setInverse <-function(solveMatrix)
+  getInverse <- funtion () s
 
 
-## Write a short comment describing this function
+## This function create an inverse of a matrix and returned
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+  s <- x$getInverse(...)
+  if(!is.null(s)){
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data)
+  x$setInverse(s)
+} 
+    ## Return a matrix that is the inverse of 'x'    
